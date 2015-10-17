e9539d686fda3a4573181b33ef59e2f839deeab1
Sebastian Wozny <swozn@dolby.com>
2015-10-17 14:58:43 +0200

    The last time I tried this the monkey didn't survive. Let's hope it works better this time.

---
+++ apps/quests/migrations/0001_initial.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Quest',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=200, null=True, blank=True)),
+                ('description', models.TextField(null=True, blank=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Response',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('quest', models.ForeignKey(to='quests.Quest')),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='quest',
+            name='users',
+            field=models.ManyToManyField(to=settings.AUTH_USER_MODEL, null=True, through='quests.Response', blank=True),
+        ),
+    ]
