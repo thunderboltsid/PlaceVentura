15a7bf16cea695997a77874ede68710fb3784683
Sebastian Wozny <swozn@dolby.com>
2015-10-17 13:06:18 +0200

    pointless limitation

--- common/static/common/app.js
+++ common/static/common/app.js
@@ -3,8 +3,6 @@
 // Declare app level module which depends on views, and components
 angular.module('eestec', [
     'eestec.common',
-    'eestec.events',
-    'eestec.teams',
     'eestec.accounts',
     'ngMdIcons'
 ]);
