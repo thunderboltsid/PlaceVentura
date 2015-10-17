3da825a6f27c2c9f6c7d79d43300b62ca69aa518
vagrant <vagrant@ubuntu-14.04-amd64-vbox>
2015-10-17 18:16:30 +0000

    Range validator

--- apps/quests/models.py
+++ apps/quests/models.py
@@ -2,6 +2,7 @@ from django.db.models import Model, TextField, CharField, ManyToManyField, Forei
 
 __author__ = 'Sebastian Wozny'
 import logging
+import math
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -24,3 +25,23 @@ class Quest(Model):
     lng = FloatField(blank=True,null=True)
     precision = FloatField(blank=True,null=True) #TODO: Insert a default here.
 
+
+def in_range(position, answer, accuracy):
+    #radius in km
+    R = 6371
+    dlat = to_rad(position[0] - answer[0])
+    dlon = to_rad(position[1] - answer[1])
+
+    a = math.sin(dlat/2)**2 + math.cos(to_rad(position[0])) * \
+        math.cos(to_rad(answer[0])) * math.sin(dlon/2)**2
+    c = 2 * math.atan2(math.sqrt(a),math.sqrt(1-a))
+    d = R*c
+
+    #distance in meters
+    d *= 1000
+    if d<=accuracy:
+        return True
+    return False
+
+def to_rad(val):
+    return val * math.pi / 180
--- apps/quests/tests.py
+++ apps/quests/tests.py
@@ -7,6 +7,7 @@ from apps.quests.factories import QuestFactory
 
 __author__ = 'Sebastian Wozny'
 import logging
+import models
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -41,3 +42,14 @@ def test_quest_detail_accessible():
     c=Client()
     response=c.get(url)
     assert response.status_code==200, "Quest detail was unreachable"
+
+
+def test_in_range():
+    data = [
+        ((53.170389, 8.645187), (53.169071, 8.642344), 300, True),
+        ((53.166739, 8.637598), (53.165079, 8.644067), 400, False),
+        ((53.194558, 8.615379), (53.201110, 8.601758), 1000, False)
+    ]
+
+    for item in data:
+        assert models.in_range(item[0],item[1],item[2]) == item[3], "In range fails"
