479ec4428318e627ec8111de2d301fb87d6cea08
Sebastian Wozny <swozn@dolby.com>
2015-10-17 16:00:32 +0200

    Does anyone read this? I'll be at the coffee shop accross the street.

--- apps/quests/models.py
+++ apps/quests/models.py
@@ -7,7 +7,7 @@ import logging
 logger = logging.getLogger(__name__)
 class Response(Model):
     user = ForeignKey('accounts.Account')
-    quest= ForeignKey('quests.Quest')
+    quest = ForeignKey('quests.Quest')
 class Quest(Model):
     title = CharField(max_length=200,null=True,blank=True)
     description = TextField(null=True,blank=True)
