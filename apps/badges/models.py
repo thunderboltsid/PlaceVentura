63464eb54d5a624a69a2e8cd12dfa9893802bffa
Sebastian Wozny <swozn@dolby.com>
2015-10-18 00:24:59 +0200

    k

--- apps/badges/models.py
+++ apps/badges/models.py
@@ -6,4 +6,4 @@ class Badge(Model):
     name = CharField(max_length=64, null=True, blank=True)
     description = CharField(max_length=160, null=True, blank=True)
     thumbnail = ImageField(upload_to="users",null=True,blank=True,default="/media/cvs/example.jpg")
-    user = ManyToManyField("accounts.Account")
+    user = ManyToManyField("accounts.Account",null=True,blank=True)
